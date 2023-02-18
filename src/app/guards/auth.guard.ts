85198bd603f1b6644a5a3c2f57548606c0123a8b
Matias0019 <matiromero0019@gmail.com>
2023-02-17 22:49:31 -0300

    productos listos

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,9 +3,9 @@ import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStat
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate {
 
   constructor(public auth: AuthService, public router: Router) {}
